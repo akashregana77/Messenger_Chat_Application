095fac1abef356395301f9862642a710d5040ed0
akashregana77 <akashreganaoff@gmail.com>
2026-06-28 15:49:08 +0530

    imported the clerk

--- frontend/chat_app/src/main.jsx
+++ frontend/chat_app/src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
-import {BrowerRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {ClerkProvider} from "@clerk/react"
 
 createRoot(document.getElementById('root')).render(
-  <BrowerRouter>
+  
     <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowerRouter>
+      <ClerkProvider>
+        <App />
+      </ClerkProvider>
+    </StrictMode>,
 )
